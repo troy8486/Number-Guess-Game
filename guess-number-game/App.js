a189607f8650517192073d645b3ec4f566551df8
troy8486 <rtanmoy262@gmail.com>
2023-11-22 19:11:40 +0530

    msg: styling button, adding backgroundImage

--- guess-number-game/App.js
+++ guess-number-game/App.js
@@ -1,10 +1,30 @@
-import { StyleSheet } from 'react-native';
+import { StyleSheet, View, ImageBackground } from 'react-native';
 import StartGameScreen from './screens/StartGameScreen';
 
 export default function App() {
   return (
-    <StartGameScreen/>
+
+      <View style={styles.rootScreen}> 
+        <ImageBackground 
+          style={styles.imageBackground}
+          source={require('./assets/background.png')}
+          resizeMode='cover'
+        >
+          <StartGameScreen/>
+        </ImageBackground>
+      </View>
   );
 }
+
+const styles = StyleSheet.create({
+  rootScreen: {
+    backgroundColor: '#ddb52f',
+    flex: 1
+  },
+  imageBackground: {
+    flex: 1,
+    opacity: 0.8
+  }
+})
 
 
