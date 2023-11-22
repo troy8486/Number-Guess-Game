9e7c565369c43a3bd9ae5f6f86b53210c871e5fe
troy8486 <rtanmoy262@gmail.com>
2023-11-22 18:45:35 +0530

    msg: Styling and implementing components

---
+++ guess-number-game/screens/StartGameScreen.js
@@ -0,0 +1,41 @@
+import { View, TextInput, StyleSheet } from "react-native";
+import PrimaryButton
+ from "../components/PrimaryButton";
+
+function StartGameScreen(){
+    return(
+        <View style={styles.inputContainer}>
+            <TextInput style={styles.numberInput} maxLength={2}/>
+            <PrimaryButton>Reset</PrimaryButton>
+            <PrimaryButton>Confirm</PrimaryButton>
+        </View>
+    )
+}
+export default StartGameScreen;
+
+const styles = StyleSheet.create({
+    inputContainer: {
+        backgroundColor: '#720639',
+        marginHorizontal: 24,
+        borderRadius: 8,
+        elevation: 4,
+        shadowColor: 'black',
+        shadowOffset: {width: 0,height: 2},
+        shadowRadius: 6,
+        shadowRadius: 0.5,
+        marginTop: 100,
+        padding: 16,
+        alignItems: 'center'
+    },
+    numberInput: {
+        height: 50,
+        fontSize: 32,
+        borderBottomColor: '#ddb52f',
+        borderBottomWidth: 2,
+        color: '#ddb52f',
+        marginVertical: 8,
+        fontWeight: 'bold',
+        width: 50,
+        textAlign: 'center',
+    }
+})
